aade5fabafb5a58174b63cf1f3aa40dc7c97faba
istrav <travis.burandt@gmail.com>
2022-07-26 17:20:31 -0500

    machine

---
+++ src/components/Machine.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { onMount } from "svelte";
+  import Machine from '../machine/index'
+
+  onMount(() => {
+    Machine.init('entropy')
+  })
+</script>
+
+<div id="entropy"></div>
