75b956a3ad26435b13e9b13afa1ad6870b006e7e
istrav <travis.burandt@gmail.com>
2022-07-26 21:31:21 -0500

    machine 6

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,14 +7,21 @@
 <div class="case">
   <div class="control-panel">
     <div class="content">
-      <header style="text-align: center;">
+      <div class="main-settings">
+        <a href="" class="btn btn-large"><i class="material-icons">play_arrow</i></a>
+        <a href="" class="btn btn-large"><i class="material-icons">pause</i></a>
+        <a href="" class="btn btn-large"><i class="material-icons">stop</i></a>
+      </div>
+      <header>
         <h1>EntropyMachine</h1>
         <h2>by <a href="https://istrav.com" target="_blank">istrav.com</a></h2>
       </header>
     </div>
   </div>
-  <div class="entropy">
-    <Machine />
+  <div style="border: 1em solid #333; background: #000; margin: 0 3em;">
+    <div class="entropy">
+      <Machine />
+    </div>
   </div>
 </div>
 <br />
@@ -35,17 +42,25 @@
     background-image: url("data:image/svg+xml,%3Csvg width='40' height='40' viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 0h20v20H0V0zm10 17a7 7 0 1 0 0-14 7 7 0 0 0 0 14zm20 0a7 7 0 1 0 0-14 7 7 0 0 0 0 14zM10 37a7 7 0 1 0 0-14 7 7 0 0 0 0 14zm10-17h20v20H20V20zm10 17a7 7 0 1 0 0-14 7 7 0 0 0 0 14z' fill='%23111111' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
   }
 
+  .main-settings {
+    text-align: right;
+    float: right;
+    position: relative;
+    padding: 0.5em 0;
+  }
+
   .content {
-    padding: 0.5em;
+    padding: 1em;
   }
   .control-panel h1 {
     color: #888;
     margin: 0;
+    font-size: 3em;
   }
   .control-panel h2 {
     color: #444;
     font-size: 1em;
-    margin: 0;
+    margin: 0 0 0.5em;
   }
 
   .control-panel h2 a {
@@ -59,9 +74,9 @@
   .entropy {
     height: 1000px;
     width: 1000px;
+    text-align: center;
     overflow: hidden;
     margin: 0 auto;
-    border: 1em solid #333;
     padding: 0;
     background: #000;
   }
