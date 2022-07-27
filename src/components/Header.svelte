cb58a10eb344f6daa1f4f9089ef632c35ec54026
istrav <travis.burandt@gmail.com>
2022-07-27 13:32:31 -0500

    streams randomness

---
+++ src/components/Header.svelte
@@ -0,0 +1,15 @@
+<nav>
+  <div class="nav-wrapper">
+    <ul class="left">
+      <li><a href="/balls">Balls</a></li>
+      <li><a href="/spray">Spray</a></li>
+      <li><a href="/detectors">Detectors</a></li>
+    </ul>
+    <a href="/" class="brand-logo center">Stream</a>
+    <ul class="right">
+      <li><a href="/api">API</a></li>
+      <li><a href="/settings">Settings</a></li>
+      <li><a href="/about">About</a></li>
+    </ul>
+  </div>
+</nav>
